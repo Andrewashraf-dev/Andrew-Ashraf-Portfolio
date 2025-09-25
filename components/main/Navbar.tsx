f1d950a42f3515a036dafc4afe774a00b2750379
Andrewashraf-dev <Thisisandrwashrf@gmail.com>
2025-09-25 22:42:52 +0300

    new logo

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
          <div className="ml-[10px] hidden md:flex items-center space-x-2">
   {/*Logo with spin + heartbeat*/} 
   <Image
-    src="/navlogo.png"
+    src="/navlogo2.png"
     alt="Logo"
     width={40}
     height={40}
